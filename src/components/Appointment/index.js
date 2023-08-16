a4f45ad87183cc689b0067b24b199fe55ba1375e
Hassan Issak <hassanissak@Oghma-Infinium.local>
2023-08-16 17:10:28 -0400

    Install propype

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,6 +8,10 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import useVisualMode from "hooks/useVisualMode";
 import Form from "components/Appointment/Form";
+import Status from "components/Appointment/Status";
+import Confirm from "components/Appointment/Confirm";
+import Error from "components/Appointment/Error";
+
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
@@ -23,6 +27,29 @@ export default function Appointment(props) {
     props.interview ? SHOW : EMPTY
   );
 
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+  
+    transition(SAVING);
+  
+    props
+      .bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+      .catch(error => transition(ERROR_SAVE, true));
+  }
+  
+  function destroy() {
+    transition(DELETING, true);
+    props
+      .cancelInterview(props.id)
+      .then(() => transition(EMPTY))
+      .catch(error => transition(ERROR_DELETE, true));
+  }
+  
+
   return (
     <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
@@ -34,7 +61,37 @@ export default function Appointment(props) {
     student={props.interview.student}
     interviewer={props.interview.interviewer}
   />)}
-  {mode === CREATE && (<Form interviewer={props.interviewers} />)}
+{mode === CREATE && (
+  <Form
+    interviewers={props.interviewers}
+    onCancel={back}
+    onSave={save}
+  />
+)}
+{mode === EDIT && (
+  <Form
+    name={props.interview.student}
+    interviewer={props.interview.interviewer.id}
+    interviewers={props.interviewers}
+    onCancel={back}
+    onSave={save}
+  />
+)}
+{mode === SAVING && <Status message="Saving" />}
+{mode === DELETING && <Status message="Deleting" />}
+{mode === CONFIRM && (
+  <Confirm
+    message="Are you sure you would like to delete?"
+    onCancel={back}
+    onConfirm={destroy}
+  />
+)}
+{mode === ERROR_SAVE && (
+  <Error message="Could not book appointment." onClose={back} />
+)}
+{mode === ERROR_DELETE && (
+  <Error message="Could not cancel appointment." onClose={back} />
+)}
 
 
     </article>
